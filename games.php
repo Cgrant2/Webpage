f7b091b51500ed5b6a66d56b5440e41a885b48c7
Cgrant2 <connorgrant920@gmail.com>
2025-01-08 12:49:56 -0500

    adding original page and initialized games page

---
+++ games.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Darkmoon Games
+		</title>
+		<link rel="icon" type="image/x-icon" href="./webpagestuff/Favicon.png" />
+		<style>
+		.basicText {
+			font-size:30px;
+			color:#000000;
+		}
+		.navBar {
+			overflow:hidden;
+			background-color:#414A6A;
+			position: fixed;
+			top:0;
+			width:99%;
+			height:40px;
+			box-shadow: 0 0 5px 2px #000000;
+		}
+		.boxLink {
+			border-width:1px; 
+			border-style:solid; 
+			border-color:#000000; 
+			padding:5px;
+		}
+		.wrapper {
+			max-width:90%;
+			min-width:1000px;
+			min-height:1500px;
+			padding-top:40px;
+			margin:auto;
+			background-color:#616183;
+			box-shadow: 0 0 10px 2px #000000;
+		}
+		a:link {
+			color:#000000;
+			background-color:transparent;
+			text-decoration:none;
+		}
+		a:visited {
+			color:#000000;
+			background-color:transparent;
+			text-decoration:none;
+		}
+		a:hover {
+			color:#003FBF;
+			background-color:transparent;
+			text-decoration:none;
+		}
+		</style>
+	</head>
+
+	<body bgcolor="#FDFDFD">
+		<div class=navBar align=left>
+			<a href="./home.php" 
+			title="Home" style="font-size:30px; padding:16px;">
+				<span class=boxLink>Home</span>
+			</a>	
+			<a href="https://lab46.g7n.org/journal/fall2024/cgrant9/start" 
+			title="Journal" style="font-size:30px; padding:16px;">
+				<span class=boxLink>Journal</span>
+			</a>	
+		</div>
+		<br>
+		<div class=wrapper align="center">
+			<h1 style="font-size:40px; color:#000000; padding:5px;">
+				<span style="border-width:3x; border-style:solid; border-color:#000000; padding:10px;">
+					&emsp;TEMPORARY PAGE TO BE UPDATED&emsp;
+				</span>
+			</h1>
+		</div>
+	</body>
+</html>
---
+++ home.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Darkmoon Development
+		</title>
+		<link rel="icon" type="image/x-icon" href="./webpagestuff/Favicon.png" />
+		<style>
+		.basicText {
+			font-size:30px;
+			color:#000000;
+		}
+		.navBar {
+			overflow:hidden;
+			background-color:#414A6A;
+			position: fixed;
+			top:0;
+			width:99%;
+			height:40px;
+			box-shadow: 0 0 5px 2px #000000;
+		}
+		.boxLink {
+			border-width:1px; 
+			border-style:solid; 
+			border-color:#000000; 
+			padding:5px;
+		}
+		.wrapper {
+			max-width:90%;
+			min-width:1000px;
+			min-height:1500px;
+			padding-top:40px;
+			margin:auto;
+			background-color:#616183;
+			box-shadow: 0 0 10px 2px #000000;
+		}
+		a:link {
+			color:#000000;
+			background-color:transparent;
+			text-decoration:none;
+		}
+		a:visited {
+			color:#000000;
+			background-color:transparent;
+			text-decoration:none;
+		}
+		a:hover {
+			color:#003FBF;
+			background-color:transparent;
+			text-decoration:none;
+		}
+		</style>
+	</head>
+
+	<body bgcolor="#FDFDFD">
+		<div class=navBar align=left>
+			<a href="./games.php" 
+			title="Games" style="font-size:30px; padding:16px;">
+				<span class=boxLink>Games</span>
+			</a>	
+			<a href="https://lab46.g7n.org/journal/fall2024/cgrant9/start" 
+			title="Journal" style="font-size:30px; padding:16px;">
+				<span class=boxLink>Journal</span>
+			</a>	
+		</div>
+		<br>
+		<div class=wrapper align="center">
+			<h1 style="font-size:40px; color:#000000; padding:5px;">
+				<span style="border-width:3x; border-style:solid; border-color:#000000; padding:10px;">
+					&emsp;Darkmoon&emsp;
+				</span>
+			</h1>
+			<p style="font-size:20px; color:#000000; line-height:0;">
+				(Formally Connor)
+			</p>
+			<br>
+			<p style="font-size:10px; color:#000000;">
+				Music of the week:
+			</p>
+			<iframe width="150" height="150" 
+			src="https://www.youtube.com/embed/Se1uh3PS78Y?autoplay=1">
+			</iframe>
+			<br>
+			<br>
+			<br>
+			<p style="font-size:25px; color:#000000">
+				Greetings, I am Darkmoon, a 2nd year CS student
+			</p>
+		</div>
+	</body>
+</html>
